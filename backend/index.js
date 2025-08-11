23ceca6a4a55fed16f2fd1189e292deccaf9693d
Your Name <bhogilranjit@gmail.com>
2025-08-11 23:35:59 +0530

    Your descriptive commit message

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const authRoute = require("./Routes/AuthRoute");
 
 app.use(bodyParser.json());
 app.use(cors({
-  origin: "https://investra.onrender.com", // exact frontend origin
+  origin: "https://investra-1.onrender.com", // exact frontend origin
   credentials: true               // allow cookies/auth headers
 }));
 
